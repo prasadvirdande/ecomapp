08dbfd8a8f869962d497fe06805a62187acc30eb
prasad-virdande <prasad@ahhaa.com>
2025-11-17 19:19:14 +0530

    Initial branch commit

--- ecomapp/src/main/java/com/ecomapp/Controller/ProductController.java
+++ ecomapp/src/main/java/com/ecomapp/Controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.ecomapp.Controller;
 
+import com.ecomapp.DTOs.ProductResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/products")
 public class ProductController {
+
+    public ProductResponse getAllProducts() {
+
+    }
 
 
 }
---
+++ ecomapp/src/main/java/com/ecomapp/DTOs/ProductResponse.java
@@ -0,0 +1,16 @@
+package com.ecomapp.DTOs;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductResponse {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private String category;
+    private String imageUrl;
+}
